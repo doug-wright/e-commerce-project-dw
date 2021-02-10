a15cb25312cac720903ecc49b3e87edb2d78d6c3
Doug Wright <dw@nof49.ca>
2021-02-10 11:14:56 -0500

    Initial backend built out

---
+++ server/handlers.js
@@ -0,0 +1,148 @@
+const companies = require('./data/companies.json');
+const products = require('./data/items.json');
+
+// Get company by id
+const getCompanyById = (id) => {
+  return new Promise((resolve, reject) => {
+    const company = companies.filter(company => company._id === Number(id));
+
+    if (company.length > 0) {
+      resolve({ company: company[0] });
+    } else {
+      reject({ request: id, message: 'Company not found' });
+    }
+  });
+};
+
+// Get all products
+const getProducts = () => {
+  return new Promise((resolve, reject) => {
+    resolve(products.map(product => ({
+      name: product.name,
+      price: product.price,
+      imageSrc: product.imageSrc,
+      numInStock: product.numInStock
+    })));
+  });
+};
+
+// Get product count
+const getNumProducts = () => {
+  return new Promise((resolve, reject) => {
+    resolve({ numProducts: products.length });
+  });
+};
+
+// Get product categories
+const getProductCategories = () => {
+  return new Promise((resolve, reject) => {
+    const categories = [];
+
+    products.forEach(product => {
+      if (categories.indexOf(product.category) === -1) {
+        categories.push(product.category);
+      }
+    });
+
+    resolve({ categories });
+  });
+};
+
+// Get product by category
+const getProductsByCategory = (category) => {
+  return new Promise((resolve, reject) => {
+    const product = products.filter(product => product.category.toLocaleLowerCase() === category.toLocaleLowerCase());
+
+    if (product.length > 0) {
+      resolve({ products: product });
+    } else {
+      reject({ request: category, message: 'No products found by that category' });
+    }
+  });
+};
+
+// Get product body locations
+const getProductBodyLocations = () => {
+  return new Promise((resolve, reject) => {
+    const bodyLocations = [];
+
+    products.forEach(product => {
+      if (bodyLocations.indexOf(product.body_location) === -1) {
+        bodyLocations.push(product.body_location);
+      }
+    });
+
+    resolve({ bodyLocations });
+  });
+};
+
+// Get products by body location
+const getProductsByBodyLocation = (bodyLocation) => {
+  return new Promise((resolve, reject) => {
+    const product = products.filter(product => product.body_location.toLocaleLowerCase() === bodyLocation.toLocaleLowerCase());
+
+    if (product.length > 0) {
+      resolve({ products: product });
+    } else {
+      reject({ request: bodyLocation, message: 'No products found by that body location' });
+    }
+  });
+};
+
+// Get product by id
+const getProductById = (id) => {
+  return new Promise((resolve, reject) => {
+    const product = products.filter(product => product._id === Number(id));
+
+    if (product.length > 0) {
+      resolve({ product: product[0] });
+    } else {
+      reject({ request: id, message: 'Product not found' });
+    }
+  });
+};
+
+// Get product by index
+const getProductByIndex = (fromIndex) => {
+  return new Promise((resolve, reject) => {
+    const numProducts = products.length;
+
+    if (fromIndex < 0) {
+      reject({ request: fromIndex, message: 'invalid index' });
+    }
+
+    if (fromIndex > numProducts - 1) {
+      reject({ request: fromIndex, message: 'index exceeds number of products' });
+    }
+
+    const productRange = [];
+    let toIndex = fromIndex + 10;
+  
+    if (toIndex > numProducts - 1) {
+      toIndex = numProducts -1;
+    }
+
+    for (let i = fromIndex; i <= toIndex; i++) {
+      productRange.push({
+        name: products[i].name,
+        price: products[i].price,
+        imageSrc: products[i].imageSrc,
+        numInStock: products[i].numInStock
+      });
+    }
+
+    resolve({ products: productRange });
+  });
+};
+
+module.exports = {
+  getCompanyById,
+  getProducts,
+  getNumProducts,
+  getProductCategories,
+  getProductsByCategory,
+  getProductBodyLocations,
+  getProductsByBodyLocation,
+  getProductById,
+  getProductByIndex
+};
--- server/index.js
+++ server/index.js
@@ -6,11 +6,24 @@ const morgan = require('morgan');
 
 const PORT = 4000;
 
+const {
+  getCompanyById,
+  getProducts,
+  getNumProducts,
+  getProductCategories,
+  getProductsByCategory,
+  getProductBodyLocations,
+  getProductsByBodyLocation,
+  getProductById,
+  getProductByIndex
+} = require('./handlers');
+const { response } = require('express');
+
 express()
   .use(function(req, res, next) {
     res.header(
       'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+      'OPTIONS, HEAD, GET, PUT, PATCH, POST, DELETE'
     );
     res.header(
       'Access-Control-Allow-Headers',
@@ -25,6 +38,104 @@ express()
   .use('/', express.static(__dirname + '/'))
 
   // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+  
+  // Get company by id
+  .get('/api/v1/company/:id', async (req, res) => {
+    try {
+      const response = await getCompanyById(req.params.id);
+
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get all products
+  .get('/api/v1/product', async (req, res) => {
+    try {
+      const response = await getProducts();
+
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get product count
+  .get('/api/v1/product/count', async (req, res) => {
+    try {
+      const response = await getNumProducts();
+
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get product categories
+  .get('/api/v1/product/categories', async (req, res) => {
+    try {
+      const response = await getProductCategories();
+
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get products by category
+  .get('/api/v1/product/category/:category', async (req, res) => {
+    try {
+      const response = await getProductsByCategory(req.params.category);
+  
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get product body locations
+  .get('/api/v1/product/locations', async (req, res) => {
+    try {
+      const response = await getProductBodyLocations();
+
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get products by body location
+  .get('/api/v1/product/location/:bodyLocation', async (req, res) => {
+    try {
+      const response = await getProductsByBodyLocation(req.params.bodyLocation);
+  
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get product by id
+  .get('/api/v1/product/:id', async (req, res) => {
+    try {
+      const response = await getProductById(req.params.id);
+
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
+
+  // Get product by index
+  .get('/api/v1/product/index/:fromIndex', async (req, res) => {
+    try {
+      const response = await getProductByIndex(req.params.fromIndex);
+  
+      res.status(200).json({ status: 200, data: response });
+    } catch (err) {
+      res.status(400).json({ status: 400, data: { request: err.request, message: err.message }});
+    }
+  })
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
